751509f801ac3ba4790bf917f056120665fe0cea
TheSlavaHero <75801633+TheSlavaHero@users.noreply.github.com>
2021-01-20 22:43:07 +0100

    Update Saver.java
    Переделал так, чтобы метод помещался в 30 строк, потому что вспомнил про это в последний момент)

--- Saver.java
+++ Saver.java
@@ -2,15 +2,12 @@ package com.company;
 
 import java.io.*;
 import java.lang.reflect.*;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Saver implements Serializable {
     public static void save(Class<?>... ls) {
         try {
             for (Class<?> cls : ls) {
                     ClassWithFields cwf = new ClassWithFields();
-//                  Save save = cls.getAnnotation(Save.class);
                     Field[] fields = cls.getDeclaredFields();
                     for (Field field : fields) {
                         field.setAccessible(true);
@@ -21,21 +18,12 @@ public class Saver implements Serializable {
                         if (!save.i()) {//если i-false, то выполняем следуюищй блок кода-сериализация
                             try (ObjectOutputStream OOS = new ObjectOutputStream(new FileOutputStream(directory))) {
                                 OOS.writeObject(field.get(cwf).toString());
-                                System.out.println(field.get(cwf).toString());
-
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            } catch (IllegalAccessException e) {
-                                e.printStackTrace();
-                            }
+                            } catch (IOException | IllegalAccessException e) {e.printStackTrace();}
                         } else {//если i-true, то выполняем следуюищй блок кода-десериализация
                             try (ObjectInputStream OIS = new ObjectInputStream(new FileInputStream(directory))) {
                                 String text = (String) OIS.readObject();
                                 field.set(cwf, text);
-                                System.out.println(field.get(cwf).toString());
-                            } catch (IOException | ClassNotFoundException e) {
-                                e.printStackTrace();
-                            } catch (IllegalAccessException e) {
+                            } catch (IOException | ClassNotFoundException | IllegalAccessException e) {
                                 e.printStackTrace();
                             }
                         }
